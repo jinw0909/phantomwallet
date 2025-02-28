0d8666a479d594c1cc8d01f663c7b6e9e7a3e9b0
Jinwoo Chang <83801879+jinw0909@users.noreply.github.com>
2025-02-28 20:04:49 +0900

    250228 added metamask connection

--- src/App.js
+++ src/App.js
@@ -3,12 +3,36 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import DesktopApp from './DesktopApp';
 import MobilePhantomConnection from './MobilePhantomConnection';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { http, WagmiProvider, createConfig } from 'wagmi';
+import { mainnet, linea, lineaSepolia } from "wagmi/chains";
+import { metaMask } from 'wagmi/connectors';
+import MetaMaskApp from "./MetaMaskApp"; // Your wagmi config
+
+const config = createConfig({
+    ssr: true,
+    chains: [mainnet, linea, lineaSepolia],
+    connectors: [metaMask()],
+    transports: {
+        [mainnet.id]: http(),
+        [linea.id]: http(),
+        [lineaSepolia.id]: http(),
+    },
+});
+const queryClient = new QueryClient();
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<DesktopApp />} />
             <Route path="/mobile/*" element={<MobilePhantomConnection />} />
+            <Route path="/metamask/*" element={
+                <WagmiProvider config={config}>
+                    <QueryClientProvider client={queryClient}>
+                        <MetaMaskApp/>
+                    </QueryClientProvider>
+                </WagmiProvider>}
+            />
         </Routes>
     );
 }
---
+++ src/MetaMaskApp.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import {ConnectButton} from "./components/ConnectButton";
+
+const MetaMaskApp = () => {
+    return(
+        <>
+            <h2>MetaMaskApp</h2>
+            <ConnectButton/>
+        </>
+    )
+}
+
+export default MetaMaskApp;
